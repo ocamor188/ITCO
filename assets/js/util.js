9473a9b430b74ce9fb2f69570092cd86c2b705c6
Carlos Cascante Villalobos <ccascante87@gmail.com>
2015-10-06 20:09:29 -0600

    Adding bootstrap and pages refactor

--- assets/js/util.js
+++ assets/js/util.js
@@ -1,3 +1,58 @@
+$(function() {
+	$("li").click(function(event) {
+		// remove classes from all
+		$("li").removeClass("active");
+		// add class to the one we clicked
+		$(this).addClass("active");
+		event.preventDefault();
+		// navigation bar
+		if($(this).attr('id') == "home") {
+			$("#mainContainer").load("mision.html");
+		}
+		if($(this).attr('id') == "services") {
+			$("#mainContainer").load("servicios.html", function() {
+				callbackServicios();
+			});
+		}
+		if($(this).attr('id') == "classes") {
+			$("#mainContainer").load("capacitacion.html", function() {
+				callbackCapacitacion();
+			});
+		}
+		if($(this).attr('id') == "benefits") {
+			$("#mainContainer").load("beneficios.html");
+		}
+		if($(this).attr('id') == "contact") {
+			$("#mainContainer").load("mision.html");
+		}
+		// footer
+		if($(this).attr('id') == "footerHome") {
+			$("#mainContainer").load("mision.html");
+			$('#home').addClass("active");
+		}
+		if($(this).attr('id') == "footerServices") {
+			$("#mainContainer").load("servicios.html", function() {
+				callbackServicios();
+			});
+			$('#services').addClass("active");
+		}
+		if($(this).attr('id') == "footerClasses") {
+			$("#mainContainer").load("capacitacion.html", function() {
+				callbackCapacitacion();
+			});
+			$('#classes').addClass("active");
+		}
+		if($(this).attr('id') == "footerBenefits") {
+			$("#mainContainer").load("beneficios.html");
+			$('#benefits').addClass("active");
+		}
+		if($(this).attr('id') == "footerContact") {
+			$("#mainContainer").load("mision.html");
+			$('#contact').addClass("active");
+		}
+	});
+});
+
 function callbackServicios() {
 	$(".accordion-desc").fadeOut(0);
 	$(".accordion").click(function() {
